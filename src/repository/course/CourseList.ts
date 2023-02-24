cba37d3aeb60d28d5af24021ccc955ae2f4e8fd6
Alisson Luis <alissonluis2009@gmail.com>
2023-02-23 23:11:12 -0300

    added courses

--- src/repository/course/CourseList.ts
+++ src/repository/course/CourseList.ts
@@ -2,91 +2,116 @@ import { Course } from "../../types/Course";
 
 export default <Course[]>[
     {
-        title: 'Sla',
-        desc: 'Alguma coisa',
-        duration: '5 minutos',
-        stars: 4.0,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
-    },
-    {
-        title: 'Sla #2',
-        desc: 'Alguma outra coisa',
-        duration: '3 minutos',
+        title: 'Roteirista de Hentai',
+        desc: '',
+        duration: '38 segundos',
         stars: 5.0,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        thumb: '/img/cursos/senai-10.jpg',
+        url: 'https://www.youtube.com/watch?v=sXbGxqa2waE'
     },
     {
-        title: 'Sla #3',
-        desc: 'Alguma outra coisa - O retorno',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Agiotagem',
+        desc: '',
+        duration: '35 segundos',
+        stars: 4.5,
+        thumb: '/img/cursos/senai-1.jpg',
+        url: 'https://www.youtube.com/watch?v=buoMMFtqNxU0'
     },
     {
-        title: 'Sla #4',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Como ser Chavoso',
+        desc: '',
+        duration: '57 segundos',
+        stars: 5.0,
+        thumb: '/img/cursos/senai-2.png',
+        url: 'https://www.youtube.com/watch?v=pwngQ42RVTo'
     },
     {
-        title: 'Sla #5',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Jogo do Bicho',
+        desc: '',
+        duration: '21 segundos',
+        stars: 5.0,
+        thumb: '/img/cursos/senai-8.jpg',
+        url: 'https://www.youtube.com/watch?v=jZQR2BvRkn8'
+        
     },
     {
-        title: 'Sla #6',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Trocador de Preço de Posto',
+        desc: '',
+        duration: '47 segundos',
+        stars: 3.5,
+        thumb: '/img/cursos/senai-4.jpg',
+        url: 'https://www.youtube.com/watch?v=OfkWOKF8Jes'
     },
     {
-        title: 'Sla #6',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Transporte de Tabaco',
+        desc: '',
+        duration: '1 minutos',
+        stars: 3.8,
+        thumb: '/img/cursos/senai-5.jpg',
+        url: 'https://www.youtube.com/watch?v=Sy58BAG1g1U'
     },
     {
-        title: 'Sla #6',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Árbitro de Rinha de Aspirador Robô',
+        desc: '',
+        duration: '36 segundos',
+        stars: 3.0,
+        thumb: '/img/cursos/senai-6.jpg',
+        url: 'https://www.youtube.com/watch?v=TmWJa0cVZvM'
     },
     {
-        title: 'Sla #6',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Narração de Bingos',
+        desc: '',
+        duration: '30 segundos',
+        stars: 2.8,
+        thumb: '/img/cursos/senai-7.jpg',
+        url: 'https://www.youtube.com/shorts/SV9IBWmoclE'
     },
     {
-        title: 'Sla #6',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
+        title: 'Tecladista de Churrascaria',
+        desc: '',
+        duration: '42 segundos',
+        stars: 3.5,
+        thumb: '/img/cursos/senai-3.jpg',
+        url: 'https://www.youtube.com/watch?v=PNmLEqgGme4'
     },
     {
-        title: 'Sla #6',
-        desc: 'Alguma outra coisa - O retorno novamente',
-        duration: '3 minutos',
-        stars: 3.4,
-        thumb: 'https://placehold.co/600x400',
-        url: '#'
-    }
+        title: 'Bolar Baseado',
+        desc: '',
+        duration: '45 segundos',
+        stars: 4.2,
+        thumb: '/img/cursos/senai-9.jpg',
+        url: 'https://www.youtube.com/watch?v=KlEERme9hBk'
+    },
+    {
+        title: 'Manutenção de Caça-Níquel',
+        desc: '',
+        duration: '11 segundos',
+        stars: 3.2,
+        thumb: '/img/cursos/senai-11.jpg',
+        url: 'https://www.youtube.com/watch?v=5S-VaL6oFm4'
+    },
+    {
+        title: 'Adestramento de Papagaios',
+        desc: '',
+        duration: '20 segundos',
+        stars: 4.6,
+        thumb: '/img/cursos/senai-12.jpg',
+        url: 'https://www.youtube.com/shorts/Yh6wVQFC1lc'
+    },
+    {
+        title: 'Fabricação de Cachorro Motoserra',
+        desc: '',
+        duration: '1 minuto',
+        stars: 3.0,
+        thumb: '/img/cursos/senai-13.png',
+        url: 'https://www.youtube.com/watch?v=4LfVJDWnz-4'
+    },
+    {
+        title: 'Miojo Turbinado',
+        desc: '',
+        duration: '23 segundos',
+        stars: 4.0,
+        thumb: '/img/cursos/senai-14.jpg',
+        url: 'https://www.youtube.com/watch?v=nt9Zhzwc5Lk'
+    },
 ];
